63f27c9dac5beb92050d571194e0a89a09681b2b
sajid <sajidhasan673@gmail.com>
2023-01-30 15:09:48 +0600

    save and retrieve images from file manager

--- AmarCoin/AmarCoin/Services/CoinImageService.swift
+++ AmarCoin/AmarCoin/Services/CoinImageService.swift
@@ -14,6 +14,9 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    
+    private let fileManeger = LocalFileManager.instance
+    private let folderName = "coin_images"
 
     
     init(coin: CoinModel) {
@@ -22,6 +25,16 @@ class CoinImageService {
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManeger.getImage(imageName: coin.id, folderName: folderName) {
+            //retrived savedImage form file maneger
+            self.image = savedImage
+        } else {
+            // else, download image
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else {
             return
         }
@@ -31,8 +44,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManeger.saveImage(image: downloadedImage, imageName: self.coin.id, folderName: self.folderName)
             })
     }
 }
